a634f62bd6583b7b121b4ed8aea812c90d302251
unknown <Brennan.artzt@gmail.com>
2017-08-13 18:32:13 -0500

    bux fix 4, Improve efficiency and difficulty increase

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,5 @@
 var wordList = ["abruptly","absurd","abyss","affix","askew","avenue","awkward","axiom","azure","bagpipes","bandwagon","banjo","bayou","beekeeper","bikini","blitz","blizzard","boggle","bookworm","boxcar","boxful","buckaroo","buffalo","buffoon","buxom","buzzard","buzzing","buzzwords","caliph","cobweb","cockiness","croquet","crypt","curacao","cycle","daiquiri","dirndl","disavow","dizzying","duplex","dwarves","embezzle","equip","espionage","euouae","exodus","faking","fishhook","fixable","fjord","flapjack","flopping","fluffiness","flyby","foxglove","frazzled","frizzled","fuchsia","funny","gabby","galaxy","galvanize","gazebo","giaour","gizmo","glowworm","glyph","gnarly","gnostic","gossip","grogginess","haiku","haphazard","hyphen","iatrogenic","icebox","injury","ivory","ivy","jackpot","jaundice","jawbreaker","jaywalk","jazziest","jazzy","jelly","jigsaw","jinx","jiujitsu","jockey","jogging","joking","jovial","joyful","juicy","jukebox","jumbo","kayak","kazoo","keyhole","khaki","kilobyte","kiosk","kitsch","kiwifruit","klutz","knapsack","larynx","lengths","lucky","luxury","lymph","marquis","matrix","megahertz","microwave","mnemonic","mystify","naphtha","nightclub","nowadays","numbskull","nymph","onyx","ovary","oxidize","oxygen","pajama","peekaboo","phlegm","pixel","pizazz","pneumonia","polka","pshaw","psyche","puppy","puzzling","quartz","queue","quips","quixotic","quiz","quizzes","quorum","razzmatazz","rhubarb","rhythm","rickshaw","schnapps","scratch","shiv","snazzy","sphinx","spritz","squawk","staff","strength","strengths","stretch","stronghold","stymied","subway","swivel","syndrome","thriftless","thumbscrew","topaz","transcript","transgress","transplant","triphthong","twelfth","twelfths","unknown","unworthy","unzip","uptown","vaporize","vixen","vodka","voodoo","vortex","voyeurism","walkway","waltz","wave","wavy","waxy","wellspring","wheezy","whiskey","whizzing","whomever","wimpy","witchcraft","wizard","woozy","wristwatch","wyvern","xylophone","yachtsman","yippee","yoked","youthful","yummy","zephyr","zigzag","zigzagging","zilch","zipper","zodiac","zombie"];
-var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+var hasWordUsed = [];
 var names = ['Slender Man','The Tall Man','The Thin Man','Der Schlanker Mann','Fear Dubh','Schlankwald','Slendy','Slenderman','The Pale One','The White King','Master',' The Black King']
 var pages=0;
 var wins= document.getElementById('Wins');
@@ -8,11 +8,14 @@ var lettersUsed =document.getElementById('Letters-Used');
 var lives= document.getElementById('Lives');
 var thePage=document.getElementById('The-Page');
 var slenderman=document.getElementById('Slenderman');
-var distance = 100;
+var distance = 80;
 wins.innerHTML = "<p>You've collected " + pages + " of the eight pages</p>";
 slenderman.innerHTML= '<img class="resize" src="assets/images/Slenderman.png">';
 var gameBoard=[""];
 var playedLetters =[];
+for (var i = 0; i < wordList.length; i++) {
+	hasWordUsed.push(false);
+}
 setAnswer();
 slendy();
 function slendy()
@@ -23,13 +26,19 @@ function slendy()
 function setAnswer()
 {
 	var Board='';
-	answer=wordList[Math.floor(Math.random() * wordList.length)];
+	var random=0;
+	var tempTrue=true;
+	while(hasWordUsed[random]||tempTrue)
+	{
+		random=Math.floor(Math.random() * wordList.length);
+		answer=wordList[random];
+		tempTrue=false;
+	}
 	for (var i = 0; i < answer.length; i++) {
 		gameBoard[i]="-";
 		Board = Board+ gameBoard[i]+' ';
 	}
 	currentWord.innerHTML = "<p>" + Board + "</p>";
-	//return newAnswer;
 }
 function reset (didWin)
 {
@@ -55,7 +64,7 @@ function reset (didWin)
 	{
 		pages=0;
 		wins.innerHTML = "<p>You've collected " + pages + " of the eight pages</p>";
-		distance=100;
+		distance=80;
 		thePage.innerHTML = "<p>Slenderman got you, better luck next time</p>"
 		slenderman.innerHTML= '<img class="resize" src="assets/images/Slenderman.png">';
 	}
@@ -63,21 +72,20 @@ function reset (didWin)
 	lettersUsed.innerHTML = ""
 	slendy();
 }
-document.onkeyup = function(event) {
+document.onkeyup = function(event) 
+{
 	var isGoodInput=false;
 	var userGuess = event.key;
 	var isInWord=false;
 	var hasWon=true;
 	var Board='';
 	var lettersOutput='';
-	for (var i = 0; i < alphabet.length; i++) {
-		if(!isGoodInput&&alphabet[i]===userGuess)
-		{
-			isGoodInput=true;
-		}
+	if(userGuess.charCodeAt(0)<=122&&userGuess.charCodeAt(0)>=97)
+	{
+		isGoodInput=true;
 	}
 	for (var i = 0; i < playedLetters.length; i++) {
-		if(playedLetters[i]===userGuess&&isGoodInput)
+		if(isGoodInput&&playedLetters[i]===userGuess)
 		{
 			isGoodInput=false;
 		}
