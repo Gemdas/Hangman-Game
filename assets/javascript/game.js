4cff44de5a804a303b8b9b1df75bcd8115506bf1
unknown <Brennan.artzt@gmail.com>
2017-08-14 16:34:56 -0500

    added efficiency and actual solution to final puzzle added to the readme

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,5 @@
 var wordList = ["abruptly","absurd","abyss","affix","askew","avenue","awkward","axiom","azure","bagpipes","bandwagon","banjo","bayou","beekeeper","bikini","blitz","blizzard","boggle","bookworm","boxcar","boxful","buckaroo","buffalo","buffoon","buxom","buzzard","buzzing","buzzwords","caliph","cobweb","cockiness","croquet","crypt","curacao","cycle","daiquiri","dirndl","disavow","dizzying","duplex","dwarves","embezzle","equip","espionage","euouae","exodus","faking","fishhook","fixable","fjord","flapjack","flopping","fluffiness","flyby","foxglove","frazzled","frizzled","fuchsia","funny","gabby","galaxy","galvanize","gazebo","giaour","gizmo","glowworm","glyph","gnarly","gnostic","gossip","grogginess","haiku","haphazard","hyphen","iatrogenic","icebox","injury","ivory","ivy","jackpot","jaundice","jawbreaker","jaywalk","jazziest","jazzy","jelly","jigsaw","jinx","jiujitsu","jockey","jogging","joking","jovial","joyful","juicy","jukebox","jumbo","kayak","kazoo","keyhole","khaki","kilobyte","kiosk","kitsch","kiwifruit","klutz","knapsack","larynx","lengths","lucky","luxury","lymph","marquis","matrix","megahertz","microwave","mnemonic","mystify","naphtha","nightclub","nowadays","numbskull","nymph","onyx","ovary","oxidize","oxygen","pajama","peekaboo","phlegm","pixel","pizazz","pneumonia","polka","pshaw","psyche","puppy","puzzling","quartz","queue","quips","quixotic","quiz","quizzes","quorum","razzmatazz","rhubarb","rhythm","rickshaw","schnapps","scratch","shiv","snazzy","sphinx","spritz","squawk","staff","strength","strengths","stretch","stronghold","stymied","subway","swivel","syndrome","thriftless","thumbscrew","topaz","transcript","transgress","transplant","triphthong","twelfth","twelfths","unknown","unworthy","unzip","uptown","vaporize","vixen","vodka","voodoo","vortex","voyeurism","walkway","waltz","wave","wavy","waxy","wellspring","wheezy","whiskey","whizzing","whomever","wimpy","witchcraft","wizard","woozy","wristwatch","wyvern","xylophone","yachtsman","yippee","yoked","youthful","yummy","zephyr","zigzag","zigzagging","zilch","zipper","zodiac","zombie"];
-var hasWordUsed = [];
-var names = ['Slender Man','The Tall Man','The Thin Man','Der Schlanker Mann','Fear Dubh','Schlankwald','Slendy','Slenderman','The Pale One','The White King','Master',' The Black King']
+var names = ['Slender Man','The Tall Man','The Thin Man','Der Schlanker Mann','Fear Dubh','Schlankwald','Slendy','Slenderman','The Pale One','The White King','Master',' The Black King'];
 var pages=0;
 var wins= document.getElementById('Wins');
 var currentWord =document.getElementById('Current-Word');
@@ -13,9 +12,6 @@ wins.innerHTML = "<p>You've collected " + pages + " of the eight pages</p>";
 slenderman.innerHTML= '<img class="resize" src="assets/images/Slenderman.png">';
 var gameBoard=[""];
 var playedLetters =[];
-for (var i = 0; i < wordList.length; i++) {
-	hasWordUsed.push(false);
-}
 setAnswer();
 slendy();
 function slendy()
@@ -26,13 +22,10 @@ function slendy()
 function setAnswer()
 {
 	var Board='';
-	var random=0;
-	var tempTrue=true;
-	while(hasWordUsed[random]||tempTrue)
+	answer=undefined;
+	while(answer===undefined)
 	{
-		random=Math.floor(Math.random() * wordList.length);
-		answer=wordList[random];
-		tempTrue=false;
+		answer=wordList[Math.floor(Math.random() * wordList.length)];
 	}
 	for (var i = 0; i < answer.length; i++) {
 		gameBoard[i]="-";
@@ -44,9 +37,9 @@ function reset (didWin)
 {
 	gameBoard=[""];
 	playedLetters=[];
-	//distance=12;
 	if(didWin)
 	{
+		delete wordList[wordList.indexOf(answer)];
 		pages++;
 		if (pages<8)
 		{
